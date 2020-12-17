c876cb40185347ba43ee3bf3cec51642c4fcc556
Will Passidomo <wpassidomo@mparticle.com>
2020-12-17 15:04:50 -0500

    Update version to 1.1.0

--- src/mParticle.Sdk/Client/Configuration.cs
+++ src/mParticle.Sdk/Client/Configuration.cs
@@ -21,7 +21,7 @@ namespace mParticle.Client
         /// Version of the package.
         /// </summary>
         /// <value>Version of the package.</value>
-        public const string Version = "1.0.1";
+        public const string Version = "1.1.0";
 
         /// <summary>
         /// Identifier for ISO 8601 DateTime Format
