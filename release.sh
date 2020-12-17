b577c561a2d08350c524afb79fd129fe70a8faed
Will Passidomo <wpassidomo@mparticle.com>
2020-12-17 12:45:52 -0500

    Add Release script

---
+++ release.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+: ${1?"Version missing - usage: $0 x.y.z"}
+
+git pull origin master
+
+
+#update Configuration.cs
+sed  -i '' "s/public const string Version = \".*\"/public const string Version = \"$1\"/g" src/mParticle.Sdk/Client/Configuration.cs
+
+#update csproj
+sed  -i '' "s/\<Version\>.*\</\<Version\>$1\</g" src/mParticle.Sdk/mParticle.Sdk.csproj
+
+#commit the version bump, tag, and push to private and public
+git add src/mParticle.Sdk/Client/Configuration.cs
+git add src/mParticle.Sdk/mParticle.Sdk.csproj
+git commit -m "Update version to $1"
+git tag "v$1"
+git push origin "v$1"
+git push origin HEAD:master
